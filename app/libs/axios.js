cea28a1b5adb1b2e2d5db2c0b828136d7a7784df
itzDeepansu <itzdeepansu@gmail.com>
2025-02-23 15:26:23 +0530

    axios+prisma bug

--- app/libs/axios.js
+++ app/libs/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
 const API = axios.create({
-    // baseURL:"https://water-level-nine.vercel.app/api"
-    baseURL:"http://192.168.0.181:3000/api"
+    //production
+    baseURL:"https://water-level-nine.vercel.app/api"
+    //Development
+    // baseURL:"http://192.168.0.181:3000/api"
 })
 
 export default API
